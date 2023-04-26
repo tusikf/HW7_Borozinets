2f4de602f4b12a3bb020a38b1a6e7b784f0d1d3a
CEOmeleshenko <wwwhhhgtbsbe@gmail.com>
2023-04-26 22:00:19 +0300

    add m8_quiz_animation

---
+++ m8_quiz_animation/app/src/main/java/com/example/quiz/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.quiz.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWelcomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_quizFragment)
+        }
+    }
+}
