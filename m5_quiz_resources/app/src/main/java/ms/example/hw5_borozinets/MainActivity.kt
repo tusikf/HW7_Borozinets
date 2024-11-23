7994e828492f8f81c22e74fb32f929986b8a9a34
Nati <tusikf@yandex.ru>
2024-11-23 20:31:51 +0300

    done

--- m5_quiz_resources/app/src/main/java/ms/example/hw5_borozinets/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/ms/example/hw5_borozinets/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.quiz.R
 import com.example.quiz.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +22,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
